564a9f59530679f55b308677ea2e43304c47454b
Peter Stein <peterstein@protonmail.com>
2018-12-18 15:35:12 +0100

    Fix singleton for Python 2

--- runregcrawlr/singleton.py
+++ runregcrawlr/singleton.py
@@ -11,6 +11,13 @@
 # or submit itself to any jurisdiction.
 
 
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import super
+from future import standard_library
+standard_library.install_aliases()
 class Singleton(type):
     """
     Allow only one instance of a class
